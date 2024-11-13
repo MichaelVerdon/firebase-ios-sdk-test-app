536adf3cfc05b48e1088c16113d69fa9e713348e
MichaelVerdon <mwverdon2000@gmail.com>
2024-11-13 14:29:26 +0000

    Initial Commit

---
+++ firebase-test-app-ios/firebase_test_app_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  firebase_test_app_iosApp.swift
+//  firebase-test-app-ios
+//
+//  Created by Michael Verdon on 13/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct firebase_test_app_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
